dccb915a7c6b54aa0741471a7160cd0de5ec4f2c
1297276320 <93326923+1297276320@users.noreply.github.com>
2025-08-29 11:14:49 +0800

    Add files via upload

---
+++ talk.js
@@ -0,0 +1,43 @@
+$(function() {
+    $('#yes').click(function(event) {
+        modal('我就知道小宝宝一定会开心的~(^_^)', function() {
+            $('.page_one').addClass('hide');
+            $('.page_two').removeClass('hide');
+            // typeWrite();
+            fireworks();
+
+        });
+    });
+    $('#no').click(function(event) {
+        modal('明人不说暗话！', A);
+    });
+});
+
+function A() {
+    modal('好吧0.0', J);
+}
+function J() {
+    modal('那我们看烟花吧~', function() {
+        fireworks();
+    });
+}
+
+function fireworks() {
+    $('.page_one').addClass('hide');
+    initAnimate();
+}
+
+function modal(content, callback) {
+    var tpl = '<div class="container">'+
+        '<div class="mask"></div>'+
+        '<div class="modal">'+
+        '<p>'+ content +'</p>'+
+        '<button type="button" id="confirm" class="confirm">确定</button>'+
+        '</div>'+
+        '</div>';
+    $('body').append(tpl);
+    $(document).on('click', '.confirm', function() {
+        $('.container').remove();
+        callback();
+    });
+}
